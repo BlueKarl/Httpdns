82cc9b7edb6792d05ea112ccbb592946958f4058
BlueKarl <karl@e.hunantv.com>
2016-02-17 15:15:39 +0800

    update to v2.0.1 for test

--- app.lua
+++ app.lua
@@ -6,18 +6,30 @@ local stat = require "stat"
 
 local rds = redis:new()
 
+local function get_errcode()
+    if config.SWITCH ~= 0 then
+        errcode = 102
+    else
+        errcode = 0
+    end
+    return errcode
+end
+
 local function get_domains()
-    local domains, _ = cache:get("__domains__")
-    if not domains then
-        domains, err = rds:smembers(common.HTTPDNS_DOMAIN)
-        if err ~= nil then
-            ngx.log(ngx.ERR, err)
-            ngx.exit(ngx.HTTP_BAD_REQUEST)
-        end
+    if errcode == 0 then
+        local domains, _ = cache:get("__domains__")
         if not domains then
-            ngx.exit(ngx.HTTP_NOT_FOUND)
+            domains, err = rds:smembers(common.HTTPDNS_DOMAIN)
+            if err ~= nil then
+                errcode = 101
+                ngx.log(ngx.ERR, err)
+                ngx.exit(ngx.HTTP_BAD_REQUEST)
+            end
+            if not domains then
+                ngx.exit(ngx.HTTP_NOT_FOUND)
+            end
+            cache:set("__domains__", domains, config.DEFAULT_TTL * 2)
         end
-        cache:set("__domains__", domains, config.DEFAULT_TTL * 2)
     end
     return domains
 end
@@ -25,22 +37,32 @@ end
 local domains = get_domains()
 
 local function get_remote_ip()
-    local headers = ngx.req.get_headers()
-    local x_forwarded_for = headers['x-forwarded-for']
-    local x_real_ip = headers['x-real-ip']
-
+    local args = ngx.req.get_uri_args()
+    local flag = 0
     local remote = ngx.var.arg_eip
-    if not remote and x_forwarded_for then
-        local s, _ = string.find(x_forwarded_for, ", ")
-        if s then
-            remote = string.sub(x_forwarded_for, 1, s-1)
-        else
-            remote = x_forwarded_for
+    for name, data in pairs(args) do
+        if name == "cip" then
+            remote = data
+            flag = 1
         end
-    elseif not remote and x_real_ip then
-        remote = x_real_ip
-    else
-        remote = ngx.var.remote_addr
+    end
+    if flag ~= 1 then
+        local headers = ngx.req.get_headers()
+        local x_forwarded_for = headers['x-forwarded-for']
+        local x_real_ip = headers['x-real-ip']
+
+        if not remote and x_forwarded_for then
+            local s, _ = string.find(x_forwarded_for, ", ")
+            if s then
+                remote = string.sub(x_forwarded_for, 1, s-1)
+            else
+                remote = x_forwarded_for
+            end
+        elseif not remote and x_real_ip then
+            remote = x_real_ip
+        else
+            remote = ngx.var.remote_addr
+        end
     end
     return remote
 end
@@ -49,14 +71,15 @@ local function get_sp_num(remote)
     local long_ip = string.format('%d', utils.ip2long(remote))
     local ip_range_id, err = rds:zrangebyscore(common.HTTPDNS_IP_RANGE, long_ip, '+inf', 'limit', 0, 1)
     if err ~= nil then
+        errcode = 101
         ngx.log(ngx.ERR, err)
         ngx.exit(ngx.HTTP_BAD_REQUEST)
     end
-
     local sp_num = common.HTTPDNS_DEFAULT_SP
     if ip_range_id ~= nil then
         sp_num, err = rds:hget(string.format(common.HTTPDNS_PROVIDER, ip_range_id[1]), 'isp')
         if err ~= nil then
+            errcode = 101
             ngx.log(ngx.ERR, err)
             ngx.exit(ngx.HTTP_BAD_REQUEST)
         end
@@ -64,58 +87,71 @@ local function get_sp_num(remote)
     return tonumber(sp_num)
 end
 
-local function get_domains_info(sp_num)
+string.split = function(s, p)
+    local rt= {}
+    string.gsub(s, '[^'..p..']+', function(w) table.insert(rt, w) end )
+    return rt
+end
+
+local function get_domains_test(sp_num)
     local cache_key = string.format(common.HTTPDNS_SP_CACHE, sp_num)
-    local data, _ = cache:get(cache_key)
-    local domains_info = {}
+    local data, _ = cache:get(cache_key) 
+    local args = ngx.req.get_uri_args()
+    local domains_name = {}
     if not data then
-        for index, domain in ipairs(domains) do
-            local default_hosts_key = string.format(common.HTTPDNS_DOMAIN_SP, domain, common.HTTPDNS_DEFAULT_SP)
-            local default_hosts, domain_hosts, err
-            default_hosts, err = rds:smembers(default_hosts_key)
-            if sp_num ~= common.HTTPDNS_DEFAULT_SP and err == nil then
-                local domain_hosts_key = string.format(common.HTTPDNS_DOMAIN_SP, domain, sp_num)
-                domain_hosts, err = rds:smembers(domain_hosts_key)
-            end
-            if not err then
-                local hosts_info = {}
-                local count = 1
-                if default_hosts then
-                    for _, ip in ipairs(default_hosts) do
-                        hosts_info[count] = {priority=config.DEFAULT_HOST_PRIORITY, ip=ip}
-                        count = count + 1
+        local default_hosts, domain_hosts, err
+        for k,v in pairs(args) do
+            if k == "dn" then
+                local domain_list = string.split(v, ',')
+                for _, domain_choose in ipairs(domain_list) do
+                    local default_hosts_key = string.format(common.HTTPDNS_DOMAIN_SP, domain_choose, common.HTTPDNS_DEFAULT_SP)
+                    default_hosts, err = rds:smembers(default_hosts_key)
+                    if sp_num ~= common.HTTPDNS_DEFAULT_SP and err == nil then
+                        local domain_hosts_key = string.format(common.HTTPDNS_DOMAIN_SP, domain_choose, sp_num)
+                        domain_hosts, err = rds:smembers(domain_hosts_key)
+                    end
+                    if not err then
+                        local hosts_info = {}
+                        local count = 1
+                        if default_hosts then
+                            for _, ip in ipairs(default_hosts) do
+                                hosts_info[count] = {priority=config.DEFAULT_HOST_PRIORITY, ip=ip}
+                                count = count + 1
+                            end
+                        end
+                        if domain_hosts then
+                            for _, ip in ipairs(domain_hosts) do
+                                hosts_info[count] = {priority=config.MATCHED_HOST_PRIORITY, ip=ip}
+                                count = count + 1
+                            end
+                        end
+                        domains_name[_] = {dn=domain_choose, data=hosts_info, ttl=config.DEFAULT_TTL}
+                    else
+                    ngx.log(ngx.ERR, err)
                     end
                 end
-                if domain_hosts then
-                    for _, ip in ipairs(domain_hosts) do
-                        hosts_info[count] = {priority=config.MATCHED_HOST_PRIORITY, ip=ip}
-                        count = count + 1
-                    end
-                end
-                domains_info[index] = {domain=domain, dns=hosts_info}
-            else
-                ngx.log(ngx.ERR, err)
             end
         end
         cache:set(cache_key, domains_info, config.DEFAULT_TTL)
     else
-        domains_info = data
+        domains_name = data
     end
-    return domains_info
-end
+    return domains_name
+end 
 
 ngx.timer.at(0, stat.total_request_count)
 local remote = get_remote_ip()
 local sp_num = get_sp_num(remote)
-local domains_info = get_domains_info(sp_num)
-
+local k = get_domains_test(sp_num)
+local code = get_errcode()
 local result = {}
-result['device_ip'] = remote
-result['device_isp'] = sp_num
-result['ttl'] = config.DEFAULT_TTL
-result['domains'] = domains_info
+
+result['errcode'] = code
+if errcode == 0 then
+    result['content'] = k
+else 
+    result['content'] = ''
+end
 
 ngx.timer.at(0, stat.sp_request_count, sp_num)
 ngx.say(cjson.encode(result))
-
-
--- config.lua
+++ config.lua
@@ -4,6 +4,7 @@ _M.REDIS_HOST = os.getenv("REDIS_HOST") and os.getenv("REDIS_HOST") or 'localhos
 _M.REDIS_PORT = os.getenv("REDIS_PORT") and os.getenv("REDIS_PORT") or '6379'
 _M.DEFAULT_TTL = tonumber(os.getenv("DEFAULT_TTL")) and tonumber(os.getenv("DEFAULT_TTL")) or 60
 _M.DEFAULT_HOST_PRIORITY = tonumber(os.getenv("DEFAULT_HOST_PRIORITY")) and tonumber(os.getenv("DEFAULT_HOST_PRIORITY")) or 0
-_M.MATCHED_HOST_PRIORITY = tonumber(os.getenv("MATCHED_HOST_PRIORITY")) and tonumber(os.getenv("MATCHED_HOST_PRIORITY")) or 30
+_M.MATCHED_HOST_PRIORITY = tonumber(os.getenv("MATCHED_HOST_PRIORITY")) and tonumber(os.getenv("MATCHED_HOST_PRIORITY")) or 5
+_M.SWITCH = tonumber(os.getenv("SWITCH")) and tonumber(os.getenv("SWITCH")) or 0
 
 return _M
