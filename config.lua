bdabfbc11ac8cf372baec03639bc4fb7aeb3a4a6
BlueKarl <karl@e.hunantv.com>
2016-05-11 11:01:39 +0800

    modify CONFIG.DEFAULT_TTl

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ local _M = {}
 
 _M.REDIS_HOST = os.getenv("REDIS_HOST") and os.getenv("REDIS_HOST") or 'localhost'
 _M.REDIS_PORT = os.getenv("REDIS_PORT") and os.getenv("REDIS_PORT") or '6379'
-_M.DEFAULT_TTL = tonumber(os.getenv("DEFAULT_TTL")) and tonumber(os.getenv("DEFAULT_TTL")) or 60
+_M.DEFAULT_TTL = tonumber(os.getenv("DEFAULT_TTL")) and tonumber(os.getenv("DEFAULT_TTL")) or 300
 _M.DEFAULT_HOST_PRIORITY = tonumber(os.getenv("DEFAULT_HOST_PRIORITY")) and tonumber(os.getenv("DEFAULT_HOST_PRIORITY")) or 0
 _M.MATCHED_HOST_PRIORITY = tonumber(os.getenv("MATCHED_HOST_PRIORITY")) and tonumber(os.getenv("MATCHED_HOST_PRIORITY")) or 5
 _M.SWITCH = tonumber(os.getenv("SWITCH")) and tonumber(os.getenv("SWITCH")) or 0
